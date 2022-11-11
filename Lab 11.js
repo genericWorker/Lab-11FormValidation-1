eea43bbf5515836f1fbb0e36aa83c6aec72c49fa
mrudula-e <62666795+mrudula-e@users.noreply.github.com>
2022-11-11 13:48:42 -0500

    Update Lab 11.js

--- Lab 11.js
+++ Lab 11.js
@@ -1,48 +1,50 @@
+let em = "";
+
 function isValidEmail() {
-    em = $.trim($("input[name='email']").val());
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (emailPattern.test(em)) {
-        return true;
+   em = $.trim($("input[name='email']").val());
+   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+   if (emailPattern.test(em)) {
+       return true;
+   }
+   else {
+       $("#emailErrorMessage").html("Missing or invalid email"); 
+       return false; 
+   }
+}
+ function doPasswordsMatch() {
+  //  let emvalue = '<p>' + em + '</p>';
+    let password1 = $("input[name='password1']").val();
+    let password2 = $("input[name='password2']").val();
+    if ((password1 !='') && (password1 === password2)) {
+        return true; 
     }
     else {
-       // Add your code here to handle the error. 
-       alert(" Please fill all fields! Try again?");
-       return false;  
+        $('#passwordErrorMessage').html("Password missing or doesn't match");
+        return false; 
     }
  }
- 
- //var email = $('#1')[0];
-$(function() {  
-    $("#sub").on('click', function() {
-    
-    let x = document.getElementById("1").value;
-    let s = document.getElementById("2").value;
-    let d = document.getElementById("3").value; // get the current password value
-    if (s != d || $("#1").val() === "") {
-        $("h4").append("Passwords does not match!" );  
-        $("div").append($("#1"));     
-  }
-   else  {
-        $("h4").append( "Passwords matches."); 
-        $("div").append($("#1"));    
- }   
- document.getElementById("demo").innerHTML = text;
 
-    // Get the value of the input field with id="numb"
-  //let x = document.getElementById("sub").value;
+ function clearResults() {
+   $('#output').empty();
+   $('#emailErrorMessage').empty(); 
+   $('#passwordErrorMessage').empty(); 
+ }
 
-  // If x is Not a Number or less than one or greater than 10
- /* let text;
-  if (isNaN(x) || x < 1 || x > 10) {
-    text = "Passwords does not match!";
-  } else {
-    text = "Passwords matches.";
-  }
-  document.getElementById("demo").innerHTML = text;
-   */ 
-   
-   }
-)});
+ $("input").focus(function() {
+    clearResults();
+ }); 
+
+  $(document).on("submit",function(e) {
+       e.preventDefault();
+       clearResults();
+
+      if (doPasswordsMatch() && isValidEmail() ) {
+        var emvalue = '<p>' + em + '</p>';
+        $('#output').append("<p>Form output:<\p>");
+        $('#output').append( emvalue);
+        $('#output').append('Passwords match');
+      }
+  }); 
 
 
 
